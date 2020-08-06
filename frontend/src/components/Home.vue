fb0fa5ebe6fb648dafac74cac2ec5439070acfdf
gaberani <khs0783@naver.com>
2020-08-06 17:57:42 +0900

    Modify: 서버 배포용 API_URL 수정

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -73,8 +73,8 @@ import Navbar from '../components/Navbar.vue'
 import Sidebar from '../components/Sidebar.vue'
 import axios from 'axios'
 
-// const API_URL = 'http://i3a507.p.ssafy.io:8081/'
-const API_URL = 'http://localhost:8081/'
+const API_URL = 'http://i3a507.p.ssafy.io:8081/'
+// const API_URL = 'http://localhost:8081/'
 
 export default {
   name: 'Home',
