92c03d1423bdca959f404557efe05b0ec91233fc
김민하 <0311minha@gmail.com>
2020-08-11 17:06:24 +0900

    feature: search, introduce 페이지 추가

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      <v-container>
+      <Navbar></Navbar>
+      검색 페이지 입니다.
+      </v-container>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    name: 'Introduce',
+    components: {
+        Navbar,
+    },
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+    padding-top:120px
+}
+</style>
