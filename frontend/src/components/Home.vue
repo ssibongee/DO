1f4fb525a48b945d1bf33ebff11c5866b013ea75
gaberani <khs0783@naver.com>
2020-08-12 10:55:19 +0900

    Modify: 사이드바 삭제 & 컨벤션 정리

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,75 +5,71 @@
       <Navbar></Navbar>
     </div>
     <v-container>
-    <!-- 게시글 탭 -->
-    <v-tabs v-model="tab" grow class="nav">
-      <v-tab v-for="item in items" :key="item">
-        {{ item }}
-      </v-tab>
-    </v-tabs>
-    <!-- 게시글 미리보기 -->
-    <v-row>
-      <v-col>
-        <v-tabs-items v-model="tab">
-          <v-tab-item v-for="(item) in items" :key="item">
-            <v-row v-if="posts.length">
-              <!-- <div v-if="item.length"> -->
-                <v-col col="12" md="6" lg="4" v-for="(post, index) in posts" :key="index+'_posts'">
-                  <v-card
-                    hover
-                    @click="testpostdetail(post.pid)"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
-                      :src="post.thumbnail"
+      <!-- 게시글 탭 -->
+      <v-tabs v-model="tab" grow class="nav">
+        <v-tab v-for="item in items" :key="item">
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+      <!-- 게시글 미리보기 -->
+      <v-row>
+        <v-col>
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="(item) in items" :key="item">
+              <v-row v-if="posts.length">
+                <!-- <div v-if="item.length"> -->
+                  <v-col col="12" md="6" lg="4" v-for="(post, index) in posts" :key="index+'_posts'">
+                    <v-card
+                      hover
+                      @click="testpostdetail(post.pid)"
                     >
-                      <v-card-title>{{post.title}}</v-card-title>
-                    </v-img>
-
-                    <v-card-subtitle class="pb-0">{{ item }}</v-card-subtitle>
-
-                    <v-card-text class="text--primary">
-                      <div>{{post.content}}</div>
-                      <div v-if="!post.content">게시글 내용이 비어있습니다.</div>
-                      <div>작성 날짜: {{post.publishedTime}}</div>
-                    </v-card-text>
-
-                    <v-card-actions>
-                      <v-btn
-                        color="orange"
-                        text
+                      <v-img
+                        class="white--text align-end"
+                        height="200px"
+                        :src="post.thumbnail"
                       >
-                        Share
-                      </v-btn>
+                        <v-card-title>{{post.title}}</v-card-title>
+                      </v-img>
 
-                      <v-btn
-                        color="orange"
-                        text
-                      >
-                        Explore
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-col>
+                      <v-card-subtitle class="pb-0">{{ item }}</v-card-subtitle>
+
+                      <v-card-text class="text--primary">
+                        <div>{{post.content}}</div>
+                        <div v-if="!post.content">게시글 내용이 비어있습니다.</div>
+                        <div>작성 날짜: {{post.publishedTime}}</div>
+                      </v-card-text>
+
+                      <v-card-actions>
+                        <v-btn
+                          color="orange"
+                          text
+                        >
+                          Share
+                        </v-btn>
+
+                        <v-btn
+                          color="orange"
+                          text
+                        >
+                          Explore
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <v-row v-else>
+                  게시글이 없습니다.
               </v-row>
-              <v-row v-else>
-                게시글이 없습니다.
-            </v-row>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-col>
-      <v-col id="side" class="hidden-sm-and-down" cols="3">
-        <Sidebar/>
-      </v-col>
-    </v-row>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
-import Sidebar from '../components/Sidebar.vue'
 import axios from 'axios'
 
 const API_URL = 'http://i3a507.p.ssafy.io:8081/'
@@ -115,8 +111,7 @@ export default {
     })
   },
   components: {
-    Navbar,
-    Sidebar
+    Navbar
   },
   mounted() {
   },
