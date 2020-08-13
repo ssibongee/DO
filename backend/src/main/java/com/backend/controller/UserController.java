3d5fd4b5bbefe84521796ad2d000dafab004355f
doiiollo <ssibongee@gmail.com>
2020-08-13 16:43:20 +0900

    Fix : CORS 문제 해결 테스트 - 10

--- backend/src/main/java/com/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -29,7 +29,7 @@ import java.util.Optional;
 import static java.awt.SystemColor.info;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = {"Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"}, exposedHeaders = {"Access-Control-Allow-Origin"})
+@CrossOrigin(origins = "*")
 @Slf4j
 public class UserController {
 
