de29359a598c4d395639d971866cacdaf3653a30
ssibongee <ssibongee@gmail.com>
2020-08-18 14:26:18 +0900

    Feature : 이미지 제거시 디렉토리 내부에 이미지파일도 삭제

--- backend/src/main/java/com/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -341,6 +341,17 @@ public class UserController {
 
     @PutMapping("/api/v1/qr/{uid}")
     public String  deleteQR(@PathVariable String uid, @RequestBody Map<String, String> param) {
+        User user = service.findByUid(uid);
+        String path = "/home/ubuntu/dist/dist/img/" + user.getNickname() + "/qr";
+        File dir  = new File(path);
+
+        // 디렉토리 내의 모든 파일 삭제
+        File[] list = dir.listFiles();
+        for(int i = 0; i<list.length; i++) {
+            list[i].delete();
+        }
+
+
         try{
             String url = param.get("url");
             service.updateDefaultQR(uid, url);
