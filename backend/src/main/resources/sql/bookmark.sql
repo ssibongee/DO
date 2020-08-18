5215f71e8db8c7f90b0f052e843e73dc64392b26
ssibongee <ssibongee@gmail.com>
2020-08-18 16:00:05 +0900

    Fix : 북마크 등록 시간 추가 및 등록 옴최신순으로 불러읽어

--- backend/src/main/resources/sql/bookmark.sql
+++ backend/src/main/resources/sql/bookmark.sql
@@ -1,6 +1,7 @@
 create table bookmark (
     `uid` bigint not null,
     `pid` bigint not null,
+	`registartionTime` timestamp default now() not null,
     foreign key(`pid`) references post(`pid`) on delete cascade,
     foreign key(`uid`) references user(`uid`) on delete cascade
 ) engine=InnoDB charset=utf8;
