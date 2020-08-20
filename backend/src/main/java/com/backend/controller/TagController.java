d694e9e69378405617d017bd5f4b7f8a3584fa76
ssibongee <ssibongee@gmail.com>
2020-08-20 20:16:59 +0900

    Fix : WordCloud 에서 Tag 들을 표현할 때 차이가 나게 그려질 수 있도록 로직 수정

--- backend/src/main/java/com/backend/controller/TagController.java
+++ backend/src/main/java/com/backend/controller/TagController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Api(tags = {"Tag"})
 @RestController
@@ -26,11 +23,29 @@ public class TagController {
         List<Tag> tagList = tagService.loadTopTags();
         List<Map<String, String>> resultList = new ArrayList<>();
 
+        Collections.sort(tagList, new Comparator<Tag>() {
+            @Override
+            public int compare(Tag t1, Tag t2) {
+                return (int) (t1.getHits() - t2.getHits());
+            }
+        });
+
+        // 구간을 10개 구간으로 나눔 단, 태그의 총 개수가 10개가 안 될 경우 최소는 1로 보장 한다.
+        int interval = tagList.size() / 10;
+        if(interval == 0) interval += 1;
+
+        int count = 0;
+        Long size = 10L;
         for(Tag tag : tagList) {
+            count += 1;
             Map<String, String> map = new HashMap<>();
             map.put("text", tag.getName());
-            map.put("value", tag.getHits().toString() + "000");
+            map.put("value", size.toString());
             resultList.add(map);
+            if(count == interval) { // 구간이 다 채워지면 다음 구간의 크기로 변경
+                count = 0;
+                size *= 100L;
+            }
         }
 
         return resultList;
